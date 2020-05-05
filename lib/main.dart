9aee15341b20c9ddd793066f53ae11267d9c6d1e
Iñaki <ihorcada@gmail.com>
2020-05-05 17:24:38 +0200

    added bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,13 +23,29 @@ class MyMapSampleState extends State<MyMap> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: AppBar(
+        title: Text("Prueba"),
+      ),
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: CameraPosition(
           target: LatLng(40.4929200, -3.8737100),
-          zoom: 15,
+          zoom: 15, 
         ),
       ),
-    );
+      floatingActionButton: FloatingActionButton(
+          elevation: 10.0,
+          child: Icon(Icons.add),
+          onPressed: (){},
+          backgroundColor: Colors.green,
+        ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(height: 50.0),
+        color: Colors.blue,
+      ),
+      floatingActionButtonLocation: (
+        FloatingActionButtonLocation.centerDocked
+      ),
+      );
+    }
   }
-}
