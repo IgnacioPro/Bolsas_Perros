2f32bdbf13b379746c10b2f5e8109a1326aa43c5
Iñaki <ihorcada@gmail.com>
2020-05-05 15:38:06 +0200

    maps working

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Las Rozas Perruna',
+      home: MyMap(),
+    );
+  }
+}
+
+class MyMap extends StatefulWidget {
+  @override
+  State<MyMap> createState() => MyMapSampleState();
+}
+
+class MyMapSampleState extends State<MyMap> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(40.4929200, -3.8737100),
+          zoom: 15,
+        ),
+      ),
+    );
+  }
+}
